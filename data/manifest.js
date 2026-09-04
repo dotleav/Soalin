34b7ee956eed748bca8be897a160f30ad0bba529
dotleav <whyutakethisusername@gmail.com>
2026-09-04 23:10:29 +0700

    Added soal PA bergambar

--- data/manifest.js
+++ data/manifest.js
@@ -5,6 +5,15 @@
 
 export const packages = [
   {
+    "id": "2h-pa-1__2h-pa-1-latihan-pa-sist-saraf-gambar-v1",
+    "category": "2H PA 1",
+    "title": "2h-pa-1__latihan-pa-sist-saraf-gambar-v1",
+    "file": "./data/packages/2h-pa-1__2h-pa-1-latihan-pa-sist-saraf-gambar-v1/questions.js",
+    "count": 52,
+    "convertedAt": "2026-09-04T16:08:23.531Z",
+    "source": "Soal_Bergambar_PA_Sistem_Saraf.docx"
+  },
+  {
     "id": "2h-pa-1__lat-pa-sist-saraf-theory-only",
     "category": "2H PA 1",
     "title": "lat pa sist.saraf theory only",
@@ -30,6 +39,15 @@ export const packages = [
     "count": 52,
     "convertedAt": "2026-09-04T16:00:38.183Z",
     "source": "Soal_Ciri_Khas_Sediaan_PA_Sistem_Saraf.docx"
+  },
+  {
+    "id": "2h-pa-1__latihan-pa-sist-saraf-gambar-v1",
+    "category": "2H PA 1",
+    "title": "latihan-pa-sist-saraf-gambar-v1",
+    "file": "./data/packages/2h-pa-1__latihan-pa-sist-saraf-gambar-v1/questions.js",
+    "count": 52,
+    "convertedAt": "2026-09-04T16:08:54.496Z",
+    "source": "Soal_Bergambar_PA_Sistem_Saraf.docx"
   },
   {
     "id": "2h-sl-pungsi-lumbal__latihan-pretes-pungsi-lumbal",
