178511968918316ca32cbe3b95bf8b7ee050483f
dotleav <whyutakethisusername@gmail.com>
2026-09-05 20:55:28 +0700

    hapus convert.bat dan delete.ba

--- delete.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0convert.ps1" -StartTab delete
-if %errorlevel% neq 0 (
-    echo.
-    echo ERROR: PowerShell script gagal dengan kode %errorlevel%
-    echo Pastikan convert.ps1 ada di folder yang sama dengan delete.bat
-    pause
-)
